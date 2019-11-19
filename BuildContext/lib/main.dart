bad7c8b20069adcceabae167cec5a04ad90cad49
Seventy Nine <31624384+long25vn@users.noreply.github.com>
2019-11-19 13:06:33 +0700

    Update main.dart

--- BuildContext/lib/main.dart
+++ BuildContext/lib/main.dart
@@ -1,1 +1,32 @@
 
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MainApp(),
+    ));
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.ac_unit),
+          WithBuildContext(),
+        ],
+      ),
+    ));
+  }
+}
+
+class WithBuildContext extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Column column = context.ancestorWidgetOfExactType(Column);
+    return Text(column.children.length.toString(),
+        textAlign: TextAlign.center, style: TextStyle(fontSize: 28.0));
+  }
+}
