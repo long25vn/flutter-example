54aa5855227215ebbfe8bc3c825c80476123d383
Seventy Nine <31624384+long25vn@users.noreply.github.com>
2019-11-24 21:30:56 +0700

    Create main.dart

---
+++ multi_type_input/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Forms Demo',
+      home: HomeMaterial(),
+    );
+  }
+}
+
+
+class HomeMaterial extends StatefulWidget {
+  @override
+  _HomeMaterialState createState() => _HomeMaterialState();
+}
+
+class _HomeMaterialState extends State<HomeMaterial> {
+  final _formKey = GlobalKey<FormState>();
+  final _user = User();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Profile')),
+        body: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Builder(
+                builder: (context) => Form(
+                    key: _formKey,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextFormField(
+                            decoration:
+                                InputDecoration(labelText: 'First name'),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your first name';
+                              }
+                            },
+                            onSaved: (val) =>
+                                setState(() => _user.firstName = val),
+                          ),
+                          TextFormField(
+                              decoration:
+                                  InputDecoration(labelText: 'Last name'),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter your last name.';
+                                }
+                              },
+                              onSaved: (val) =>
+                                  setState(() => _user.lastName = val)),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                            child: Text('Subscribe'),
+                          ),
+                          SwitchListTile(
+                              title: const Text('Monthly Newsletter'),
+                              value: _user.newsletter,
+                              onChanged: (bool val) =>
+                                  setState(() => _user.newsletter = val)),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                            child: Text('Interests'),
+                          ),
+                          CheckboxListTile(
+                              title: const Text('Cooking'),
+                              value: _user.passions[User.PassionCooking],
+                              onChanged: (val) {
+                                setState(() =>
+                                    _user.passions[User.PassionCooking] = val);
+                              }),
+                          CheckboxListTile(
+                              title: const Text('Traveling'),
+                              value: _user.passions[User.PassionTraveling],
+                              onChanged: (val) {
+                                setState(() => _user
+                                    .passions[User.PassionTraveling] = val);
+                              }),
+                          Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 16.0),
+                              child: RaisedButton(
+                                  onPressed: () {
+                                    final form = _formKey.currentState;
+                                    if (form.validate()) {
+                                      form.save();
+                                      _user.save();
+                                      _showDialog(context);
+                                    }
+                                  },
+                                  child: Text('Save'))),
+                        ])))));
+  }
+
+  _showDialog(BuildContext context) {
+    Scaffold.of(context)
+        .showSnackBar(SnackBar(content: Text('Submitting form')));
+  }
+}
+
+class User {
+  static const String PassionCooking = 'cooking';
+  static const String PassionHiking = 'hiking';
+  static const String PassionTraveling = 'traveling';
+
+  String firstName = '';
+  String lastName = '';
+  Map<String, bool> passions = {
+    PassionCooking: false,
+    PassionHiking: false,
+    PassionTraveling: false
+  };
+  bool newsletter = false;
+
+  save() {
+    print('saving user using a web service');
+  }
+}
