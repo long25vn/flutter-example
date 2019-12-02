a454d1fe9ee685279e9cfc5b6c302e96d7b24ff5
Seventy Nine <31624384+long25vn@users.noreply.github.com>
2019-12-02 21:51:24 +0700

    Create future_api.dart

---
+++ dart/future_api.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+const news = '<thu thập tin tức ở đây>';
+
+Future<void> printTinTucHangNgay() async {
+  final future = thuThapTinTuc();
+  return future.then(print);
+}
+
+printTinXoSo() {
+  print('So xo: [23, 63, 87, 26, 2]');
+}
+
+printTinThoiTiet() {
+  print("Cuối tuần lạnh dưới 10 độ.");
+}
+
+printTinTheThao() {
+  print('Man Utd bổ nhiệm HLV mới');
+}
+
+main() {
+  printTinTucHangNgay();
+  printTinXoSo();
+  printTinThoiTiet();
+  printTinTheThao();
+}
+
+Future<String> thuThapTinTuc() =>
+    Future.delayed(Duration(seconds: 1), () => news);
+//https://www.dartlang.org/f/dailyNewsDigest.txt
